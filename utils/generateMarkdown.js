23a51978cf85fc4a674350f3849fe2709b0c6e06
Brice Veyna <bricev99@gmail.com>
2022-07-16 16:27:27 -0700

    Add base switch statement to renderLicenseBadge function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,19 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  const licenseBadge;
+  switch(license) {
+    case apache:
+      licenseBadge = `![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)`;
+      break;
+    case mit:
+      licenseBadge = `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`;
+      break;
+    case none:
+      licenseBadge = ``;
+      break;
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
